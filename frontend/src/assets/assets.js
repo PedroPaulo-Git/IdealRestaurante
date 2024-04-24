ab7787aa5293179dd421b155759465cbe259dd7e
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-04-24 02:11:15 -0300

    style:primeira etapa do header pronto

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,5 @@
 import BannerIdeal from './BannerIdeal.jpg';
-import Logo from './Logo.jpg'
+import Logo from './Logo.png'
 export const assets = {
     BannerIdeal,
     Logo
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { FaBasketShopping } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState('menu')
+
   return (
     <div className='navbar'>
 
       <img className='Logo' src={assets.Logo} alt="" />
 
       <ul className='navbar-menu'>
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile</li>
-        <li>contato</li>
+        <li onClick={()=>{setMenu('home')}} className={`${menu === 'home' ? 'active':''}`}>home</li>
+        <li onClick={()=>{setMenu('menu')}} className={`${menu === 'menu' ? 'active':''}`}>menu</li>
+        <li onClick={()=>{setMenu('mobile')}} className={`${menu === 'mobile' ? 'active':''}`}>mobile</li>
+        <li onClick={()=>{setMenu('contato')}} className={`${menu === 'contato' ? 'active':''}`}>contato</li>
       </ul>
 
       <div className='navbar-right'>
-        <span><FaBasketShopping /></span>
+        <span style={{fontSize:30,marginTop:2}} className='navbar-right-search-icon'><FaSearch /></span>
         <div className='navbar-right-search'>
-          <span><FaSearch /></span>
+          <span style={{fontSize:30}} className='navbar-right-basket'><FaBasketShopping /></span>
           <div className='dot'></div>
         </div>
-        <button>Registrar</button>
+        <button className='button-navbar'>Registrar</button>
       </div>
 
     </div>
