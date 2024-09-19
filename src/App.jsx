538392503b45c4e0bf7db03ddfe3be2846950f60
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-09-19 14:40:47 -0300

    qrcode pix working

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup';
+import { Buffer } from 'buffer';
+
+
 
 const App = () => {
-  const [showLoginPopup,setShowLoginPopup] = useState(false);
-
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
+  window.Buffer = Buffer;
   return (
     <>
-    {showLoginPopup?<LoginPopup setShowLoginPopup={setShowLoginPopup}/>:<></>}
-    <div className='app'>
-      <Navbar setShowLoginPopup={setShowLoginPopup}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>        
-        <Route path='/carrinho' element={<Cart/>}/>
-        <Route path='/pedido' element={<PlaceOrder/>}/>
-      </Routes>
-    </div>
-    <Footer/>
+      {showLoginPopup ? <LoginPopup setShowLoginPopup={setShowLoginPopup} /> : <></>}
+      <div className='app'>
+        <Navbar setShowLoginPopup={setShowLoginPopup} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/carrinho' element={<Cart />} />
+          <Route path='/pedido' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
+  define: {
+    'process.env': {},
+  },
   plugins: [react()],
 })
