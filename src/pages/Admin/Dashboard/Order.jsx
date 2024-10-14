072faff4100ef37427564bea9c877dae83460750
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-10-14 09:34:58 -0300

    three dots to change status order

--- src/pages/Admin/Dashboard/Order.jsx
+++ src/pages/Admin/Dashboard/Order.jsx
@@ -1,93 +1,172 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { assets } from '../../../assets/assets';
-import { StoreContext } from '../../../context/StoreContext';
+import React, { useContext, useEffect, useState } from "react";
+import { assets } from "../../../assets/assets";
+import { StoreContext } from "../../../context/StoreContext";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Order = () => {
-    const [orders, setOrders] = useState([]); // Rename state to orders
-    const { food_list } = useContext(StoreContext);
+  const [orders, setOrders] = useState([]); // Rename state to orders
+  const { food_list } = useContext(StoreContext);
+  const [openDropdownId, setOpenDropdownId] = useState(null); 
 
-    useEffect(() => {
-        const fetchOrders = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/api/orders'); // Adjust the URL as needed
-                if (!response.ok) {
-                    throw new Error('Failed to fetch orders');
-                }
-                const data = await response.json();
-                setOrders(data);
-            } catch (error) {
-                console.error('Error fetching orders:', error);
-            }
-        };
+  const toggleDropdown = (id) => {
+    
+    setOpenDropdownId((prevId) => (prevId === id ? null : id));
+  };
 
-        fetchOrders();
-    }, []);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/orders"); // Adjust the URL as needed
+        if (!response.ok) {
+          throw new Error("Failed to fetch orders");
+        }
+        const data = await response.json();
+        setOrders(data);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    };
 
-    return (
-        <div>
-            <div className="overflow-x-auto w-[100%] rounded-lg lg:min-w-[60vw] ml-5">
-                <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-                    <thead className="ltr:text-left rtl:text-right">
-                        <tr>
-                            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Pedido</th>
-                            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Endereço</th>
-                            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Valor</th>
-                            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Situação</th>
-                        </tr>
-                    </thead>
+    fetchOrders();
+  }, []);
 
-                    <tbody className="divide-y divide-gray-200">
-                        {orders.length > 0 ? (
-                            orders.map((order) => {
-                                const totalPrice = order.total.toFixed(2); // Total from the order data
+  return (
+    <div>
+      <div className="overflow-x-auto w-[100%] rounded-lg lg:min-w-[60vw] ml-5">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left rtl:text-right">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Pedido
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Endereço
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Valor
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Situação
+              </th>
+            </tr>
+          </thead>
 
-                                return (
-                                    <tr key={order.id}>
-                                        <td className="whitespace-nowrap px-4 py-0 font-medium text-gray-900">
-                                            {order.items.map((item, index) => {
-                                                const foodItem = food_list.find(food => food._id === item.productId.toString());
-                                                return (
-                                                    <div key={index} className='flex items-center my-4'>
-                                                        <img src={foodItem?.image || assets.defaultImage} alt={foodItem?.name || 'Product'} className="w-16 h-16 object-cover mr-2" />
-                                                        <div className='flex flex-col'>
-                                                            <p>{foodItem?.name || 'Unknown Product'} (x{item.quantity})</p>
-                                                        </div>
-                                                    </div>
-                                                );
-                                            })}
-                                        </td>
+          <tbody className="divide-y divide-gray-200">
+            {orders.length > 0 ? (
+              orders.map((order) => {
+                const totalPrice = order.total.toFixed(2); // Total from the order data
 
-                                        <td className="whitespace-nowrap px-4 py-2">
-                                            <div className='flex justify-center flex-col items-center'>
-                                                <p className='font-medium'>Roberto Alves</p> {/* Replace with actual client data */}
-                                                <p>Rua Lagoa Rasa Nº42</p> {/* Replace with actual address */}
-                                            </div>
-                                        </td>
+                return (
+                  <tr key={order.id}>
+                    <td className="whitespace-nowrap px-4 py-0 font-medium text-gray-900">
+                      {order.items.map((item, index) => {
+                        const foodItem = food_list.find(
+                          (food) => food._id === item.productId.toString()
+                        );
+                        return (
+                          <div key={index} className="flex items-center my-4">
+                            <img
+                              src={foodItem?.image || assets.defaultImage}
+                              alt={foodItem?.name || "Product"}
+                              className="w-16 h-16 object-cover mr-2"
+                            />
+                            <div className="flex flex-col">
+                              <p>
+                                {foodItem?.name || "Unknown Product"} (x
+                                {item.quantity})
+                              </p>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </td>
 
-                                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                            <div className='flex justify-center'>
-                                                <p>R$: {totalPrice}</p>
-                                            </div>
-                                        </td>
+                    <td className="whitespace-nowrap px-4 py-2">
+                      <div className="flex justify-center flex-col items-center">
+                        <p className="font-medium">Roberto Alves</p>{" "}
+                        {/* Replace with actual client data */}
+                        <p>Rua Lagoa Rasa Nº42</p>{" "}
+                        {/* Replace with actual address */}
+                      </div>
+                    </td>
 
-                                        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                                            <div className='flex justify-center'>
-                                                <p>{order.status}</p> {/* Displaying order status */}
-                                            </div>
-                                        </td>
-                                    </tr>
-                                );
-                            })
-                        ) : (
-                            <tr>
-                                <td colSpan="4" className="text-center py-4">No orders available</td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
-}
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      <div className="flex justify-center">
+                        <p>R$: {totalPrice}</p>
+                      </div>
+                    </td>
+
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                      <div className="flex justify-center">
+                        <p>{order.status}</p> {/* Displaying order status */}
+                      </div>
+                    </td>
+                    
+                    <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                    <BsThreeDotsVertical
+                        className="cursor-pointer text-lg"
+                        onClick={() => toggleDropdown(order.id)}
+                      />
+                      <div
+                        id="dropdown"
+                        className={`z-10 ${
+                          openDropdownId === order.id ? "block" : "hidden"
+                        } bg-white divide-y absolute right-32 divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
+                      >
+                        <ul
+                          class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                          aria-labelledby="dropdownDefaultButton"
+                        >
+                          <li>
+                            <a
+                              href="#"
+                              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            >
+                              Dashboard
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            >
+                              Settings
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            >
+                              Earnings
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href="#"
+                              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                            >
+                              Sign out
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="4" className="text-center py-4">
+                  No orders available
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Order;
