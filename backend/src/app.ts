3e03c90c8fe32fe43c7522f0e64ac029dbef1e16
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-11-01 15:05:38 -0300

    reconfig cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,21 @@
-import express from 'express';
-import clientRoutes from './routes/clienteRoutes'; 
-import adminRouter from './routes/adminRoutes';
-import stripeRoutes from './Services/StripeService'
-import cors from 'cors';
-
+import express from "express";
+import clientRoutes from "./routes/clienteRoutes";
+import adminRouter from "./routes/adminRoutes";
+import stripeRoutes from "./Services/StripeService";
+import cors from "cors";
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "https://ideal-restaurante.vercel.app", // Substitua pela origem permitida
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Inclua 'OPTIONS' aqui
+  allowedHeaders: ["Content-Type", "Authorization"], // Inclua cabeçalhos permitidos se necessário
+};
+
+// Aplicar o middleware CORS com as opções configuradas
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/api', clientRoutes);
-app.use('/api', stripeRoutes)
-app.use('/admin', adminRouter)
+app.use("/api", clientRoutes);
+app.use("/api", stripeRoutes);
+app.use("/admin", adminRouter);
 
 export default app;
